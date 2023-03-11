b21e6773f52a48844fd6ca389ee651b701526133
Alex Antropoff <alant@idc.md>
2023-03-11 09:58:56 +0200

    22

--- task4/SceneDelegate.swift
+++ task4/SceneDelegate.swift
@@ -17,9 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let ws = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: ws)
         let vc = ViewController()
         let nc = UINavigationController(rootViewController: vc)
-        window?.windowScene = ws
         window?.rootViewController = nc
         window?.makeKeyAndVisible()
     }
